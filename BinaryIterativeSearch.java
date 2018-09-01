7650a45eada494b8adc8c28ee2564b6cafc75675
LinXu0317 <35785945+LinXu0317@users.noreply.github.com>
2018-08-31 21:35:59 -0700

    Add files via upload

---
+++ BinaryIterativeSearch.java
@@ -0,0 +1,24 @@
+public  class BinaryIterativeSearch implements Practice2Search {
+
+    public String searchName() {
+
+        return null;
+    }
+    public int search(int [] arr, int target){
+        int i = 0;
+        int j = arr.length -1;
+        while(i <= j){
+            int mid = (i+j)/2;
+            if (arr[mid] == target){
+                return mid;
+            }
+            else if (arr[mid] < target){
+                i = mid +1;
+            }
+            else {
+                j = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
